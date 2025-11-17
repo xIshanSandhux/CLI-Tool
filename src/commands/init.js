d6b969e0f587e68b09c36c70cd78348fc0f03dc1
Ishan <ishan.sandhu3121@gmail.com>
2025-11-16 18:26:34 -0800

    completed init command, refactored it a lil bit

--- src/commands/init.js
+++ src/commands/init.js
@@ -3,7 +3,7 @@
 // it will create a new dir (config folder) in the user's home directory
 // it will create a new file (config.json)
 // ask user for the API key, provider, model, and cache settings and save it to the config.json file
-import { createConfigDir, createConfigFile, updateConfigFile, checkConfigDirExists, checkConfigFileExists, readConfigFile } from '../config/configMain.js';
+import { createConfigDir, createConfigFile, updateConfigFile, checkConfigDirExists, checkConfigFileExists } from '../config/configMain.js';
 import inquirer from 'inquirer';
 
 export default function initCommand(program) {
@@ -16,93 +16,65 @@ export default function initCommand(program) {
             }
             if (!checkConfigFileExists()) {
                 await createConfigFile();
-                await inquirer.prompt([
-                    {
-                        type: 'input',
-                        name: 'apiKey',
-                        message: 'Please Enter your API Key: '
-                    },
-                    {
-                        type: 'list',
-                        name: 'provider',
-                        message: 'Please Select your AI Provider',
-                        choices: ['groq', 'google'],
-                        default: 'groq'
-                    },
-                    {
-                        type: 'input',
-                        name: 'model',
-                        message: 'Please Enter your Model Name',
-                        default: 'llama3.1-8b-instant'
-                    },
-                    {
-                        type: 'confirm',
-                        name: 'cache',
-                        message: 'Do you want to enable caching?',
-                        default: false
-                    }
-                ]).then(async(userInput) =>{
-                    await updateConfigFile(userInput);
-                }).catch((error) =>{
-                    console.error(`Error initializing the CLI tool getting user input: ${error}`);
-                })
+                await askingUserQuestions();
             }else{
-                const config = await readConfigFile();
-                await inquirer.prompt([
+                const updateRequest = await inquirer.prompt([
                     {
                         type: 'confirm',
                         name: 'updateConfirmation',
                         message: 'Config file already exists. Do you want to update it?',
                         default: false
-                    },
-                    {
-                        when: (answers) => answers.updateConfirmation === true,
-                        type: 'input',
-                        name: 'apiKey',
-                        message: 'Please Enter your API Key: ',
-                        default: config.apiKey
-                    },
-                    {
-                        when: (answers) => answers.updateConfirmation === true,
-                        type: 'list',
-                        name: 'provider',
-                        message: 'Please Select your AI Provider',
-                        choices: ['groq', 'google'],
-                        default: config.provider
-                    },
-                    {
-                        when: (answers) => answers.updateConfirmation === true,
-                        type: 'input',
-                        name: 'model',
-                        message: 'Please Enter your Model Name: ',
-                        default: config.model
-                    },
-                    {
-                        when: (answers) => answers.updateConfirmation === true,
-                        type: 'confirm',
-                        name: 'cache',
-                        message: 'Do you want to enable caching?',
-                        default: config.cache
                     }
-                ]).then(async (userInput) => {
-                    if (userInput.updateConfirmation === false) {
-                        await updateConfigFile(config);
-                    }else{
-                        const updatedConfig = {
-                            apiKey: userInput.apiKey,
-                            provider: userInput.provider,
-                            model: userInput.model,
-                            cache: userInput.cache
-                        }
-                        await updateConfigFile(updatedConfig);
+                ])
+                .then(async(updateRequest) => {
+                    if(updateRequest.updateConfirmation){
+                        await askingUserQuestions();
                     }
                 }).catch((error) => {
                     console.error(`Error initializing the CLI tool getting user input: ${error}`);
                 })
             }
-            
         } catch (error) {
             console.error(`Error initializing the CLI tool: ${error}`);
         }
+    })
+}
+
+// questions for the user to answer
+function initQuestions() {
+    return [
+        {
+            type: 'input',
+            name: 'apiKey',
+            message: 'Please Enter your API Key: '
+        },
+        {
+            type: 'list',
+            name: 'provider',
+            message: 'Please Select your AI Provider: ',
+            choices: ['groq', 'google'],
+            default: 'groq'
+        },
+        {
+            type: 'input',
+            name: 'model',
+            message: 'Please Enter your Model Name',
+            default: 'llama3.1-8b-instant'
+        },
+        {
+            type: 'confirm',
+            name: 'cache',
+            message: 'Do you want to enable caching?',
+            default: false
+        }
+    ] 
+}
+
+// asking user questions and updating the config file
+async function askingUserQuestions() {
+    await inquirer.prompt(initQuestions()).then(async(userInput) =>{
+        await updateConfigFile(userInput);
+    }).catch((error) =>{
+        console.error(`Error initializing the CLI tool getting user input: ${error}`);
     })
 }
