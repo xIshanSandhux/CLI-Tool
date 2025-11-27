e51b4747e7a0910c3ac8894d88dbc7d64d7219cb
Ishan <ishan.sandhu3121@gmail.com>
2025-11-26 22:18:23 -0800

    add regex to clean the code file without breaking indentation before passing to llm

--- src/commands/explain/explainCommand.ts
+++ src/commands/explain/explainCommand.ts
@@ -4,13 +4,14 @@ import { Command } from "commander";
 import fs from 'node:fs';
 import path from 'node:path';
 
+
 export default function explainCommand(program: Command){
 
     program.command('explain')
     .description('Explain full code file or specific code block (line numbers)')
     .option('-f, --file <filePath>', 'Path to the code file to explain')
     .action(async (options)=>{
-
+        
         let systemPrompt: string = '';
         let fileContent: string = '';
         let filePath: string = '';
@@ -18,7 +19,10 @@ export default function explainCommand(program: Command){
             systemPrompt = fs.readFileSync('src/commands/explain/sysPrompt.md', 'utf8');
             filePath = path.resolve(options.file);
             fileContent = fs.readFileSync(path.resolve(options.file), 'utf8');
-            console.log(`File content: ${fileContent}`);
+            console.log(`File length before white space removal: ${fileContent.length}`);
+            const TrimmedSpaceRemoved : string = fileContent.replace(/[ \t]+$/gm,"");
+            const removeBlankLines : string = TrimmedSpaceRemoved.replace(/^\s*$/gm,"");
+            fileContent = removeBlankLines;
         }catch(error){
             console.error(`Error reading system prompt: ${error}`);
         }
@@ -30,6 +34,8 @@ export default function explainCommand(program: Command){
         console.log(options);
         if('file' in options){
             const response = await chat(options.file, messages);
+            // console.log(marked.parse(response));
+            // console.log(md.render(response));
             console.log(response);
         }else{
             console.error('No file path provided');
